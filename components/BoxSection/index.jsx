9d642f7a0b2d1f9a6791f6a489a52ab22715477d
freddymhs <freddy.huaylla@klog.co>
2023-01-21 19:43:02 -0300

    new boxx

--- components/BoxSection/index.jsx
+++ components/BoxSection/index.jsx
@@ -1,5 +1,6 @@
 
 import { useState } from "react";
+import { Dimensions, ScrollView, StyleSheet } from 'react-native';
 import {
   Button,
   FlatList,
@@ -11,73 +12,27 @@ import {
 } from "react-native";
 
 export const BoxSection = ({ mainLists }) => {
-  // const mainLists = {
-  //   salud: {
-  //     description: "mi salud",
-  //     items: [
-  //       {
-  //         name: "nutricionista",
-  //         priority: "alto",
-  //         type: "deseo",
-  //       },
-  //       {
-  //         name: "ir al oculista",
-  //         priority: "bajo",
-  //         type: "necesidad",
-  //       },
-  //       {
-  //         name: "gym",
-  //         priority: "mid",
-  //         type: "deseo",
-  //       },
-  //     ],
-  //   },
-  //   mama: {
-  //     description: "listado de cosas de mama",
-  //     items: [
-  //       {
-  //         name: "comprar regalos",
-  //         priority: "mid",
-  //         type: "deseo",
-  //       },
-  //       {
-  //         name: "oculista",
-  //         priority: "alto",
-  //         type: "deseo",
-  //       },
-  //     ],
-  //   },
-  //   casa: {
-  //     description: "listado de cosas de casa",
-  //     items: [
-  //       {
-  //         name: "fix cocina",
-  //         priority: "mid",
-  //         type: "necesidad",
-  //       },
-  //       {
-  //         name: "fix casa boby",
-  //         priority: "alto",
-  //         type: "necesidad",
-  //       },
-  //     ],
-  //   },
-  // };
-  // tendre muchas listas
-  // como sabre cual es necesidad o deseo? poreq al agregarlo se asigna
-  // como sabre su proridad ? se asigna autoamticamente bajo
-
-  // vamos a mostrar maximo 40 items por area y si sobran se puede dar en VER MAS apra ocupar toda la screen de 1 cat
-
-  // primero organizemos las listas por prioridad
-  // luego vamos rellenando cada  cuadros hasta su tope de 40
+  const screenWidth = Dimensions.get('window').width;
+  const styles = StyleSheet.create({
+    itemContainer: {
+      backgroundColor: '#e0e0e0',
+      padding: 4,
+      margin: 5,
+      borderRadius: 20,
+    },
+    itemText: {
+      fontSize: 10,
+      color: 'black',
+      textAlign: 'center',
+      width: screenWidth / 6,
+    },
+  });
   const keysInList = mainLists && Object.keys(mainLists);
 
   const formattedItemsFromLists = keysInList?.reduce(
     (accum, key) => {
       const list = mainLists[key];
-      // const hightPriorityList = list.priority === "alto";
-      // const lowPriorityList = list.priority === "bajo";
+
       list.items?.filter((item) => {
         const important = item.type === "necesidad" && item.priority === "alto";
         const midImportant =
@@ -104,156 +59,226 @@ export const BoxSection = ({ mainLists }) => {
   );
 
   return (
-    <View style={{ flex: 2, flexDirection: "row" }}>
+    <View style={{ flex: 6, flexDirection: 'column' }}>
+      <View style={{ flex: 1, flexDirection: 'row' }}>
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', width: screenWidth / 2, borderColor: 'black', borderWidth: 1 }}>
+          <Text>Esquina superior izquierda</Text>
+          <ScrollView>
 
-      <View
-        style={{
-          flex: 1,
-          // flexDirection: "column",
-          borderWidth: 1,
-          borderBottomColor: "black",
-          alignItems: "center",
-        }}
-      >
-        <Text>Necesidad</Text>
-        <View
-          style={{
-            flex: 1,
-            flexWrap: "wrap",
-            borderWidth: 1,
-            borderBottomColor: "black",
-            alignItems: "center",
-            alignContent: "center",
-            // width: "50%",
-            flexDirection: "row", //que sea column cuando sean muchos ITEMS
-          }}
-        >
-          {formattedItemsFromLists?.[0]?.map((items) => {
-            return (
-              <>
-                <TouchableOpacity
-                  style={{
-                    backgroundColor: "grey",
-                    borderRadius: 5,
-                    height: 40,
-                    padding: 10,
-                    margin: 1,
-                  }}
-                  onPress={() => alert("Botón pulsado")}
-                >
-                  <Text style={{ color: "white" }}>{items.name}</Text>
+            <FlatList
+              data={formattedItemsFromLists?.[0]}
+              numColumns={2}
+              renderItem={({ item }) => (
+                <TouchableOpacity style={styles.itemContainer}>
+                  <Text style={styles.itemText}>{item.name}</Text>
                 </TouchableOpacity>
-              </>
-            );
-          })}
+              )}
+              keyExtractor={(item) => item.name}
+            />
+
+          </ScrollView>
         </View>
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', width: screenWidth / 2, borderColor: 'black', borderWidth: 1 }}>
+          <Text>Esquina superior derecha</Text>
+          <ScrollView>
 
-        <View
-          style={{
-            flex: 1,
-            flexWrap: "wrap",
-            borderWidth: 1,
-            borderBottomColor: "black",
-            alignItems: "center",
-            alignContent: "center",
-            // width: "50%",
-            flexDirection: "row", //que sea column cuando sean muchos ITEMS
-          }}
-        >
-          {formattedItemsFromLists?.[1]?.map((items) => {
-            return (
-              <>
-                <TouchableOpacity
-                  style={{
-                    backgroundColor: "grey",
-                    borderRadius: 5,
-                    height: 40,
-                    padding: 10,
-                    margin: 1,
-                  }}
-                  onPress={() => alert("Botón pulsado")}
-                >
-                  <Text style={{ color: "white" }}>{items.name}</Text>
+            <FlatList
+              data={formattedItemsFromLists?.[1]}
+              numColumns={2}
+              renderItem={({ item }) => (
+                <TouchableOpacity style={styles.itemContainer}>
+                  <Text style={styles.itemText}>{item.name}</Text>
                 </TouchableOpacity>
-              </>
-            );
-          })}
+              )}
+              keyExtractor={(item) => item.name}
+            />
+
+          </ScrollView>
         </View>
       </View>
-      <View
-        style={{
-          flex: 1,
-          // flexDirection: "column",
-          borderWidth: 1,
-          borderBottomColor: "black",
-          alignItems: "center",
-        }}
-      >
-        <Text>Deseo</Text>
-        <View
-          style={{
-            flex: 1,
-            flexWrap: "wrap",
-            borderWidth: 1,
-            borderBottomColor: "black",
-            alignItems: "center",
-            alignContent: "center",
-            // width: "50%",
-            flexDirection: "row", //que sea column cuando sean muchos ITEMS
-          }}
-        >
-
-          {formattedItemsFromLists?.[2]?.map((items) => {
-            return (
-              <>
-                <TouchableOpacity
-                  style={{
-                    backgroundColor: "grey",
-                    borderRadius: 5,
-                    height: 40,
-                    padding: 10,
-                    margin: 1,
-                  }}
-                  onPress={() => alert("Botón pulsado")}
-                >
-                  <Text style={{ color: "white" }}>{items.name}</Text>
+      <View style={{ flex: 1, flexDirection: 'row' }}>
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', width: screenWidth / 2, borderColor: 'black', borderWidth: 1 }}>
+          <Text>Esquina inferior izquierda</Text>
+          <ScrollView>
+            <FlatList
+              data={formattedItemsFromLists?.[2]}
+              numColumns={2}
+              renderItem={({ item }) => (
+                <TouchableOpacity style={styles.itemContainer}>
+                  <Text style={styles.itemText}>{item.name}</Text>
                 </TouchableOpacity>
-              </>
-            );
-          })}
+              )}
+              keyExtractor={(item) => item.name}
+            />
+          </ScrollView>
         </View>
-        <View
-          style={{
-            flex: 1,
-            flexWrap: "wrap",
-            borderWidth: 1,
-            borderBottomColor: "black",
-            alignItems: "center",
-            alignContent: "center",
-            // width: "50%",
-            flexDirection: "row", //que sea column cuando sean muchos ITEMS
-          }}
-        >
-          {formattedItemsFromLists?.[3]?.map((items) => {
-            return (
-              <>
-                <TouchableOpacity
-                  style={{
-                    backgroundColor: "grey",
-                    borderRadius: 5,
-                    height: 40,
-                    padding: 10,
-                    margin: 1,
-                  }}
-                  onPress={() => alert("Botón pulsado")}
-                >
-                  <Text style={{ color: "white" }}>{items.name}</Text>
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', width: screenWidth / 2, borderColor: 'black', borderWidth: 1 }}>
+          <Text>Esquina inferior derecha</Text>
+          <ScrollView>
+            <FlatList
+              data={formattedItemsFromLists?.[3]}
+              numColumns={2}
+              renderItem={({ item }) => (
+                <TouchableOpacity style={styles.itemContainer}>
+                  <Text style={styles.itemText}>{item.name}</Text>
                 </TouchableOpacity>
-              </>
-            );
-          })}
+              )}
+              keyExtractor={(item) => item.name}
+            />
+          </ScrollView>
         </View>
       </View>
     </View>
+    // <View style={{ flex: 2, flexDirection: "row" }}>
+
+    //   <View
+    //     style={{
+    //       flex: 1,
+    //       // flexDirection: "column",
+    //       borderWidth: 1,
+    //       borderBottomColor: "black",
+    //       alignItems: "center",
+    //     }}
+    //   >
+    //     <Text>Necesidad</Text>
+    //     <View
+    //       style={{
+    //         flex: 1,
+    //         flexWrap: "wrap",
+    //         borderWidth: 1,
+    //         borderBottomColor: "black",
+    //         alignItems: "center",
+    //         alignContent: "center",
+    //         // width: "50%",
+    //         flexDirection: "row", //que sea column cuando sean muchos ITEMS
+    //       }}
+    //     >
+    //       {formattedItemsFromLists?.[0]?.map((items) => {
+    //         return (
+    //           <>
+    //             <TouchableOpacity
+    //               style={{
+    //                 backgroundColor: "grey",
+    //                 borderRadius: 5,
+    //                 height: 40,
+    //                 padding: 10,
+    //                 margin: 1,
+    //               }}
+    //               onPress={() => alert("Botón pulsado")}
+    //             >
+    //               <Text style={{ color: "white" }}>{items.name}</Text>
+    //             </TouchableOpacity>
+    //           </>
+    //         );
+    //       })}
+    //     </View>
+
+    //     <View
+    //       style={{
+    //         flex: 1,
+    //         flexWrap: "wrap",
+    //         borderWidth: 1,
+    //         borderBottomColor: "black",
+    //         alignItems: "center",
+    //         alignContent: "center",
+    //         // width: "50%",
+    //         flexDirection: "row", //que sea column cuando sean muchos ITEMS
+    //       }}
+    //     >
+    //       {formattedItemsFromLists?.[1]?.map((items) => {
+    //         return (
+    //           <>
+    //             <TouchableOpacity
+    //               style={{
+    //                 backgroundColor: "grey",
+    //                 borderRadius: 5,
+    //                 height: 40,
+    //                 padding: 10,
+    //                 margin: 1,
+    //               }}
+    //               onPress={() => alert("Botón pulsado")}
+    //             >
+    //               <Text style={{ color: "white" }}>{items.name}</Text>
+    //             </TouchableOpacity>
+    //           </>
+    //         );
+    //       })}
+    //     </View>
+    //   </View>
+    //   <View
+    //     style={{
+    //       flex: 1,
+    //       // flexDirection: "column",
+    //       borderWidth: 1,
+    //       borderBottomColor: "black",
+    //       alignItems: "center",
+    //     }}
+    //   >
+    //     <Text>Deseo</Text>
+    //     <View
+    //       style={{
+    //         flex: 1,
+    //         flexWrap: "wrap",
+    //         borderWidth: 1,
+    //         borderBottomColor: "black",
+    //         alignItems: "center",
+    //         alignContent: "center",
+    //         // width: "50%",
+    //         flexDirection: "row", //que sea column cuando sean muchos ITEMS
+    //       }}
+    //     >
+
+    //       {formattedItemsFromLists?.[2]?.map((items) => {
+    //         return (
+    //           <>
+    //             <TouchableOpacity
+    //               style={{
+    //                 backgroundColor: "grey",
+    //                 borderRadius: 5,
+    //                 height: 40,
+    //                 padding: 10,
+    //                 margin: 1,
+    //               }}
+    //               onPress={() => alert("Botón pulsado")}
+    //             >
+    //               <Text style={{ color: "white" }}>{items.name}</Text>
+    //             </TouchableOpacity>
+    //           </>
+    //         );
+    //       })}
+    //     </View>
+    //     <View
+    //       style={{
+    //         flex: 1,
+    //         flexWrap: "wrap",
+    //         borderWidth: 1,
+    //         borderBottomColor: "black",
+    //         alignItems: "center",
+    //         alignContent: "center",
+    //         // width: "50%",
+    //         flexDirection: "row", //que sea column cuando sean muchos ITEMS
+    //       }}
+    //     >
+    //       {formattedItemsFromLists?.[3]?.map((items) => {
+    //         return (
+    //           <>
+    //             <TouchableOpacity
+    //               style={{
+    //                 backgroundColor: "grey",
+    //                 borderRadius: 5,
+    //                 height: 40,
+    //                 padding: 10,
+    //                 margin: 1,
+    //               }}
+    //               onPress={() => alert("Botón pulsado")}
+    //             >
+    //               <Text style={{ color: "white" }}>{items.name}</Text>
+    //             </TouchableOpacity>
+    //           </>
+    //         );
+    //       })}
+    //     </View>
+    //   </View>
+    // </View>
   );
 };
