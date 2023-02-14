36ce4ea500b7029b993e37696fcac326e20f105b
freddymhs <freddy.huaylla@klog.co>
2023-02-13 22:51:19 -0300

    holaaaa

--- components/HomeSection/AddItem.jsx
+++ components/HomeSection/AddItem.jsx
@@ -29,6 +29,12 @@ export const AddItem = () => {
       placement: "top"
     })
 
+    onChangeText({
+      name: "",
+      priority: "bajo",
+      type: "deseo"
+    });
+
   }
   const [listNames, setListNames] = React.useState([]);
 
